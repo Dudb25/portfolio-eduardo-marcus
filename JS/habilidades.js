fd4f1ce6de86082d56bde27eb4ceb91220395f46
Marcus Vinicius <marcusdevinicius027@gmail.com>
2026-04-19 22:35:56 -0300

    Página habilidades
    Finalizando a estilização da página habilidades e iniciando a programação com JavaScript da mesma. Com um problema na animação de uma das setas dos tópicos que solucionarei amanhã.

---
+++ JS/habilidades.js
@@ -0,0 +1,16 @@
+function accordionList() {
+    const topicos = document.querySelectorAll('.topico');
+    const descricoes = document.querySelectorAll('.descricao');
+    topicos[0].classList.add('active');
+    topicos[0].nextElementSibling.classList.add('active');
+    topicos.forEach((topico) => {
+        topico.addEventListener(('click'), handleClick);
+    })
+    function handleClick() {
+        console.log('God is good');
+        this.classList.toggle('active');
+        this.nextElementSibling.classList.toggle('active')
+    }
+}
+
+accordionList();
