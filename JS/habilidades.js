1ab9cbffca8b295ad223269e1be2534de8e7159c
Marcus Vinicius <marcusdevinicius027@gmail.com>
2026-04-20 22:38:24 -0300

    Estilização das paginas Cursos e Sobre.
    Conclusão da parte desktop de todas as paginas exceto a de Cursos, falta colocar a imagem e aplicar as interações JavaScript.

--- JS/habilidades.js
+++ JS/habilidades.js
@@ -2,6 +2,7 @@ function accordionList() {
     const topicos = document.querySelectorAll('.topico');
     const descricoes = document.querySelectorAll('.descricao');
     topicos[0].classList.add('active');
+    topicos[4].classList.add('active');
     topicos[0].nextElementSibling.classList.add('active');
     topicos.forEach((topico) => {
         topico.addEventListener(('click'), handleClick);
